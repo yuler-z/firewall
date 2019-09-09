fc27f622ac593eb317c1e280bdb1056f769de2f2
yuler-z <zyh.p1at0@gmail.com>
2019-09-09 17:22:34 +0800

    update

--- test_mod.c
+++ test_mod.c
@@ -59,6 +59,7 @@ struct rule_node {
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("U201614817");
 
+int user_pid;
 static struct nf_hook_ops input_hook;
 static struct nf_hook_ops output_hook;
 static struct sock *nlfd = NULL; //netlink
@@ -78,7 +79,9 @@ void rcv_from_user(struct sk_buff *_skb);
 int compare_keywords(const struct keyword k1, const struct keyword k2);
 
 
-int add_hashtable(){}
+int add_hashtable(void){
+    return 0;
+}
 
 
 
@@ -176,13 +179,15 @@ int check_state_table(struct keyword kw){
     return 0; //not find in state table
 }
 int check_rule_table(const struct keyword kw){
+
+
     return 0;
 }
 
 int handle_rule_config(char* input){
     //int size = strlen(input);
-    printk("handle_rule_config");
-
+    printk("[]");
+    return 0;
 }
 
 int send_to_user(char* data){
@@ -199,39 +204,52 @@ int send_to_user(char* data){
     char input[1000];
     struct sk_buff *skb;
     struct nlmsghdr *nlh;
-    unsigned char *old_tail;
+    // unsigned char *old_tail;
     
     memset(input, '\0', 1000*sizeof(char));
     memcpy(input, data, strlen(data));
     
     size = NLMSG_SPACE(strlen(input));
     skb = alloc_skb(size, GFP_ATOMIC);
-    nlh = nlmsg_put(skb, 0, 0, 0, NLMSG_SPACE(strlen(input))-sizeof(struct nlmsghdr), 0);  //put 
-    old_tail = skb->tail;
+    if(!skb)
+	{
+	    printk("my_net_link:alloc_skb_1 error\n");
+	}
+
+    nlh = nlmsg_put(skb, 0, 0, 0, NLMSG_SPACE(strlen(input))-sizeof(struct nlmsghdr) /*size of payload*/, 0);  //put msg into skb
+    // old_tail = skb->tail;
     memcpy(NLMSG_DATA(nlh), input, strlen(input));
-    nlh->nlmsg_len = skb->tail - old_tail;
-    NETLINK_CB(skb).pid = 0;
+    //nlh->nlmsg_len = skb->tail - old_tail;
+
+    NETLINK_CB(skb).portid = 0;
     NETLINK_CB(skb).dst_group = 0;
+
     //printk(KERN_DEBUG "[kernel space] skb->data:%s\n", (char *)NLMSG_DATA((struct nlmsghdr *)skb->data));
-    retval = netlink_unicast(nlfd, skb, user_process.pid, MSG_DONTWAIT);
+    retval = netlink_unicast(nlfd, skb, user_pid, MSG_DONTWAIT);
     printk(KERN_DEBUG "[kernel space] netlink_unicast return: %d\n", retval);
     return 0;  
 }
 void rcv_from_user(struct sk_buff *__skb)
 {
     struct sk_buff *skb;
-    struct nlmsghdr *nlhdr = NULL;
+    struct nlmsghdr *nlh = NULL;
 
     skb = skb_get(__skb);
 
 	if(skb->len >= NLMSG_SPACE(0)){
 		nlh = nlmsg_hdr(skb);
+        user_pid = nlh->nlmsg_pid;
         handle_rule_config((char*)NLMSG_DATA(nlh));
 	}
     kfree_skb(skb);
 }
-static int init_module(void)
+
+int init_mod(void)
 {
+    struct netlink_kernel_cfg cfg = { 
+        .input  = rcv_from_user, /* set recv callback */
+    }; 
+
 	printk("test module loaded.\n");
 
 	// initialize input hook(pre-routing) 
@@ -253,9 +271,6 @@ static int init_module(void)
 	nf_register_net_hook(&init_net, &output_hook);
 
     // netlink create
-    struct netlink_kernel_cfg cfg = { 
-        .input  = rcv_from_user, /* set recv callback */
-    }; 
     nlfd = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
     if(!nlfd)    {
         //create failed
@@ -265,7 +280,7 @@ static int init_module(void)
 	return 0;
 }
 
-static void exit_module(void)
+void exit_mod(void)
 {
 	printk("test module exit ...\n");
 	nf_unregister_net_hook(&init_net, &input_hook); //取消钩子注册
@@ -274,5 +289,5 @@ static void exit_module(void)
 }
 
 
-module_init(init_module);
-module_exit(exit_module);
+module_init(init_mod);
+module_exit(exit_mod);
--- user.c
+++ user.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <linux/netlink.h>
@@ -36,17 +37,18 @@ int main(int argc, char* argv[])
     struct sockaddr_nl daddr; // dest socket addr
     int skfd; // the file description of netlink socket
     int ret, daddrlen = sizeof(struct sockaddr_nl);
-    struct nlmsghdr *message;
+    struct nlmsghdr *nlh;
     struct u_packet_info info;
     char *retval;
 
-    message = (struct nlmsghdr *)malloc(1);
     skfd = socket(PF_NETLINK, SOCK_RAW, NETLINK_TEST);
     if(skfd < 0){
         printf("can not create a netlink socket\n");
         return -1;
     }
-
+    printf("strlen(%ld)\n", strlen(data));
+    
+    // saddr
     memset(&saddr, 0, sizeof(saddr));
     saddr.nl_family = AF_NETLINK;
     saddr.nl_pid = getpid(); // process id
@@ -56,30 +58,34 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    //daddr
     memset(&daddr, 0, sizeof(daddr));
     daddr.nl_family = AF_NETLINK;
     daddr.nl_pid = 0;   // 发往内核，nl_pid和nl_groups设为0
     daddr.nl_groups = 0; // 
     
     // 消息头
-    memset(message, '\0', sizeof(struct nlmsghdr));
-    message->nlmsg_len = NLMSG_SPACE(strlen(data)); //length of msg
-    message->nlmsg_flags = 0; 
-    message->nlmsg_type = 0;
-    message->nlmsg_seq = 0; // sequence number
-    message->nlmsg_pid = saddr.nl_pid; // sending process PID
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(strlen(data)));
+    memset(nlh, 0, sizeof(struct nlmsghdr));
+    nlh->nlmsg_len = NLMSG_SPACE(strlen(data)); //length of msg
+    nlh->nlmsg_flags = 0; 
+    nlh->nlmsg_type = 0;
+    nlh->nlmsg_seq = 0; // sequence number
+    nlh->nlmsg_pid = saddr.nl_pid; // sending process PID
     
-    retval = memcpy(NLMSG_DATA(message), data, strlen(data));
+    memcpy(NLMSG_DATA(nlh), data, strlen(data));
     
-    printf("message sent to kernel is:\n%s\nlen:%d\n\n", (char *)NLMSG_DATA(message), message->nlmsg_len);
-    ret = sendto(skfd, message, message->nlmsg_len, 0,(struct sockaddr *)&daddr, sizeof(daddr));
+    //printf("message sent to kernel is:\n %s\n\nlen:%d", (char *)NLMSG_DATA(nlh), nlh->nlmsg_len);
+    //printf("[%d]\n", /*(char *)NLMSG_DATA(nlh)*/nlh->nlmsg_len);
+    printf("[%s]:[%d]\n", (char *)NLMSG_DATA(nlh), nlh->nlmsg_len);
+    ret = sendto(skfd, nlh, nlh->nlmsg_len, 0,(struct sockaddr *)&daddr, sizeof(daddr));
     if(!ret){
         perror("send pid:");
         exit(-1);
     }
-    
     //接受内核态确认信息
     ret = recvfrom(skfd, &info, sizeof(struct u_packet_info),0, (struct sockaddr*)&daddr, &daddrlen);
+    printf("2\n");
     if(!ret){
         perror("recv from kerner:");
         exit(-1);
@@ -87,6 +93,7 @@ int main(int argc, char* argv[])
     
     printf("message received from kernel:\n%s\n\n",((char *)info.msg));
     //内核和用户进行通信
+
     
     close(skfd);
     return 0;
