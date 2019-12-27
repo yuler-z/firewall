6e0e503ad0e02dcaa16139cd05b030c5d8140989
yuler-z <zyh.p1at0@gmail.com>
2019-12-27 15:21:39 +0800

    test:

--- user.c
+++ user.c
@@ -73,8 +73,8 @@ int rcv_from_kernel(){
     struct packet_info info;
     char *retval;
     int tag = 0;
-    // FILE *fp;
-    // fp = fopen("./firewall.log", "a");
+    FILE *fp;
+    fp = fopen("./firewall.log", "a");
 
 
     // rcv log or msg from kernel space
@@ -92,13 +92,13 @@ int rcv_from_kernel(){
         if(tag == TAG_END){
             break;
         }else if(tag == TAG_LOG){
-        //     fputs((const char*)info.msg.data, fp);   
-        // }else{
+            fputs((const char*)info.msg.data, fp);   
+        }else{
             printf("%s",(char*)info.msg.data);
         }     
     }
 
-    // fclose(fp);
+    fclose(fp);
     return 0;
 
 }
